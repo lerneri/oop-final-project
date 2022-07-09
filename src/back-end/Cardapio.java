5dd8bb5414add84429a2f974e1d448abcd30d7ab
lkgwennire <>
2022-07-09 14:27:43 +0000

    Deletadas classes Sobremesa, Principal, Entrada, Bebida

--- src/back-end/Cardapio.java
+++ src/back-end/Cardapio.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-public class Cardapio{
+public class Cardapio {
   public ArrayList<ItemCardapio> cardapio;
 
-  public Cardapio(){
+  public Cardapio() {
     cardapio = new ArrayList<ItemCardapio>();
   }
 
-  public void inserir(ItemCardapio item){
+  public void inserir(ItemCardapio item) {
     cardapio.add(item);
   }
-  
+
 }
