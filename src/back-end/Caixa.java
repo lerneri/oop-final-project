af03df27bb3932f0e59a39742c0a485112dfbd18
Victor <victorvpaiva@alu.ufc.br>
2022-07-11 19:13:28 -0300

    Apaguei os comentarios da classe caixa

--- src/back-end/Caixa.java
+++ src/back-end/Caixa.java
@@ -6,13 +6,8 @@ public class Caixa {
 	}
 	
 	public static void encerrarMesa(Mesa mesa) {
-		
-		// Quando a mesa é encerrada, o garçom continua responsável por ela
-		//TRATAR POSSIVEIS ERROS QUE ISSO IRÁ GERAR
-		//mesa.getGarcomMesa().getMesasGarcom().remove(mesa);
-		//mesa.setGarcomMesa(null);
+				
 		mesa.getPedido().clear();
-		//libera a mesa
 		mesa.setOcupada(false);
 	}
 	
