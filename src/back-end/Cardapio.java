0e8778343806e6c671a817d1cc48528b1f639475
Rodrigo Maia <rodrigonaia2003@gmail.com>
2022-07-11 11:29:56 -0300

    Construtor de Cardapio alterado

--- src/back-end/Cardapio.java
+++ src/back-end/Cardapio.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 
 import Excecoes.ItemNaoExistenteException;
@@ -5,8 +6,17 @@ import Excecoes.ItemNaoExistenteException;
 public class Cardapio {
   public static ArrayList<ItemCardapio> cardapio;
 
-  public Cardapio() {
+  public Cardapio() throws IOException {
     cardapio = new ArrayList<ItemCardapio>();
+    RandomAccessFile raf = new RandomAccessFile("cardapio.txt", "r");
+    String frase;
+    	while ((frase = raf.readLine()) != null) {
+    		ItemCardapio result=null;
+    		StringTokenizer fraseTK = new StringTokenizer(frase, ":");
+    		
+    			//result = new ItemCardapio(st[1],st[2],st[3],st[4])//converter st[4] pra double
+    	}
+    
   }
 
   public void inserir(ItemCardapio item) {
