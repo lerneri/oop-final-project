c48a0f7eeee0675f2820f08920655c7a68f5fdde
Rodrigo Maia <rodrigonaia2003@gmail.com>
2022-07-11 19:47:01 -0300

    Metodo gerarNota implementado

--- src/back-end/Caixa.java
+++ src/back-end/Caixa.java
@@ -1,8 +1,20 @@
+import java.io.*;
 import java.util.*;
 public class Caixa {
 	
-	public static void gerarNota(HashMap<ItemCardapio, Integer> pedido) {
-		
+	public static void gerarNota(HashMap<ItemCardapio, Integer> pedido) throws IOException {
+		FileWriter fw = new FileWriter("NF.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		double soma = 0;
+		for (Map.Entry <ItemCardapio, Integer> me : pedido.entrySet()) {
+			bw.write(me.getKey().getCodigo() + "  ");
+			bw.write(me.getKey().getNome() + "  ");
+			bw.write(me.getKey().getValor() + "");
+			soma += me.getKey().getValor();
+			System.lineSeparator();
+		}
+		bw.write("Valor total do pedido: " + soma);
+		bw.close();
 	}
 	
 	public static void encerrarMesa(Mesa mesa) {
--- src/back-end/Fachada.java
+++ src/back-end/Fachada.java
@@ -1,12 +1,20 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 import Excecoes.CodigoInvalidoException;
+import Excecoes.GarcomInexistenteException;
+import Excecoes.GarcomJaExistenteException;
+import Excecoes.MesaInexistenteException;
+import Excecoes.MesaJaExistenteException;
 
 public class Fachada {
 	private static Fachada instancia = null;
 	private Gerente gerente;
 	private Cardapio cardapio;
+	private ConjuntoMesas conjuntoMesas;
+	private ConjuntoGarcons conjuntoGarcons;
 
+	
 	public static Fachada getInstancia() throws IOException, CodigoInvalidoException {
 		if (instancia == null) {
 			instancia = new Fachada();
@@ -17,5 +25,8 @@ public class Fachada {
 	private Fachada() throws IOException, CodigoInvalidoException {
 		gerente = new Gerente("Fernando Trinta");
 		cardapio = new Cardapio();
+		conjuntoMesas = new ConjuntoMesas();
+		conjuntoGarcons = new ConjuntoGarcons();
 	}
+	
 }
