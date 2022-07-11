6b04ccbc5fbe6daa6f2cf11dd1e674440c6190ae
Rodrigo Maia <rodrigonaia2003@gmail.com>
2022-07-11 09:44:12 -0300

    Singleton implementado na Fachada

--- src/back-end/Fachada.java
+++ src/back-end/Fachada.java
@@ -1,9 +1,16 @@
 public class Fachada{
-  private static Fachada instancia;
+  private static Fachada instancia = null;
   private Gerente gerente;
   private Cardapio cardapio;
   
+  public static Fachada getInstancia() {
+	  if(instancia == null)
+		  instancia = new Fachada();
+	  return instancia;
+  }
+  
   private Fachada(){
-    
+    gerente = new Gerente("Fernando Trinta");
+    cardapio = new Cardapio();
   }
 }
