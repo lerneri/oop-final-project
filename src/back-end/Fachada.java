29a6063fa3a898d891d959a0fadffac28732261f
Victor <victorvpaiva@alu.ufc.br>
2022-07-11 17:38:37 -0300

    classe fachada

--- src/back-end/Fachada.java
+++ src/back-end/Fachada.java
@@ -1,16 +1,17 @@
-public class Fachada{
-  private static Fachada instancia = null;
-  private Gerente gerente;
-  private Cardapio cardapio;
-  
-  public static Fachada getInstancia() {
-	  if(instancia == null)
-		  instancia = new Fachada();
-	  return instancia;
-  }
-  
-  private Fachada(){
-    gerente = new Gerente("Fernando Trinta");
-    //cardapio = new Cardapio();
-  }
+public class Fachada {
+	private static Fachada instancia = null;
+	private Gerente gerente;
+	private Cardapio cardapio;
+
+	public static Fachada getInstancia() {
+		if (instancia == null) {
+			instancia = new Fachada();
+		}
+		return instancia;
+	}
+
+	private Fachada() {
+		gerente = new Gerente("Fernando Trinta");
+		// cardapio = new Cardapio();
+	}
 }
