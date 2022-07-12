ee3eaf97c709962e3baed5d13ff5805998ae0ea6
lerneri <leticiarneri@alu.ufc.br>
2022-07-12 18:50:32 -0300

    Tentativas na GUI

--- src/gui/Janela.java
+++ src/gui/Janela.java
@@ -23,8 +23,8 @@ public class Janela {
 					Janela janela = new Janela();
 					janela.frameInicial.setVisible(false);
 					janela.frameCaixa.setVisible(false);
-					janela.frameGerente.setVisible(false);
-					janela.frameGarcom.setVisible(true);
+					janela.frameGerente.setVisible(true);
+					janela.frameGarcom.setVisible(false);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
